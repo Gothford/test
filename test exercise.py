1bbb2f89a107c0cd84cdb5b5fc789af9338cc12b
gothford <gothford@mail.ru>
2018-03-05 04:16:41 +0400

    are you working?

---
+++ test exercise.py
@@ -0,0 +1,15 @@
+##so, this test have to success
+##and i`ll try to solve exercise here and push it in repository
+
+team_amount = int(input('teams_amount = '))
+teams = []
+for i in range(team_amount):
+    team = input('team - ')
+    score = int(input('score = '))
+    teams.append(team)
+    teams.append(score)
+print(teams)
+
+##but idk how to solve it and i wanna to sleep because time is 4:11
+##so lets try to push it in repository
+## i have added that stupid comments for test to change it tommorow
